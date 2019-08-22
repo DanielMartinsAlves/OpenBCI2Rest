cb410222403fd69eeb358a97fe8d0efbf88ff809
frontendbuilders <frontendbuilders@gmail.com>
2019-08-21 23:20:00 -0300

    ajuste encontro de porta

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ const Cyton = require('openbci-cyton');
 const k = require("@openbci/utilities").constants;// use to simulate connection
 const ourBoard = new Cyton();
 const portName = "COM3";
-/*ourBoard.listPorts()
+ourBoard.listPorts()
    .then((ports) => {
-     console.log('ports', JSON.stringify(ports));
-   });*/
-//ourBoard.connect(portName) // Port name is a serial port name, see `.listPorts()` -------- porta real
-ourBoard.connect(k.OBCISimulatorPortName) // Port name is a serial port name, see `.listPorts()` ---------- porta simulada
+     console.log('port ', ports[0].comName);
+
+ourBoard.connect(ports[0].comName) // Port name is a serial port name, see `.listPorts()` -------- porta real
+//ourBoard.connect(k.OBCISimulatorPortName) // Port name is a serial port name, see `.listPorts()` ---------- porta simulada
 
   .then(() => {
     ourBoard.streamStart();
@@ -38,9 +38,10 @@ ourBoard.connect(k.OBCISimulatorPortName) // Port name is a serial port name, se
       listRow.push(listData.length);
       for (let i = 0; i < ourBoard.numberOfChannels(); i++) {
         listRow.push(sample.channelData[i].toFixed(8));
-        console.log("Channel " + (i + 1) + ": " + sample.channelData[i].toFixed(8) + " Volts.");
+        //console.log("Channel " + (i + 1) + ": " + sample.channelData[i].toFixed(8) + " Volts.");
       }
       listRow.push(sample.boardTime);
       listData.push(listRow);
     });
+  });
 });
